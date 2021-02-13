0108b9d383bb6491130f49b656080a97edfd6481
Mehrab Hossain <hmehrab786@gmail.com>
2021-02-13 11:46:09 +0600

    Add files via upload

---
+++ app/mainjs/script.js
@@ -0,0 +1,21 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const wrapper = document.querySelector('.slider_wrap');
+const fadeElems = document.querySelectorAll('.has-fade');
+btnHamburger.addEventListener('click',function(){
+    if(header.classList.contains('open')){
+        header.classList.remove('open');
+        fadeElems.forEach(function (element) {
+            element.classList.remove('fade-int');
+            element.classList.add('fade-out');
+        });
+    }
+    else{
+        header.classList.add('open');
+        fadeElems.forEach(function (element) {
+
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
+    }
+})
